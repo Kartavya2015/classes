aee386aed4a6a510932e89296a902c06db9e175d
Kartavya2015 <smita.rajan09@gmail.com>
2025-06-19 21:37:37 +0530

    Add files via upload

---
+++ coditions/main.py
@@ -0,0 +1,19 @@
+# Program to check if a number is a natural number
+
+# Input from the user
+
+number = input("Enter a number: ")
+
+# Check if the input is a natural number
+
+
+
+number = int(number)
+
+if number > 0:
+
+ print(f"{number} is a natural number.")
+
+else : print(f"{number} is not a natural number.")
+
+  

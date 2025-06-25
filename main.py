d2e493a85dabe8b05abe2b343bde5ac2b3b5c834
Kartavya2015 <smita.rajan09@gmail.com>
2025-06-25 17:44:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = int(input("enter the number"))
-if n > 1:
-    for i in range (2,n):
-        if n%i == 0:
-            print(n, "is not a prime number")
-            break
-    else:
-        print(n,"is a prime nuber")
+def my_function():
+
+ print("Hello from a function")
+
+my_function()

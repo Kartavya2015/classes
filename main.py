6d0587cd9e6adbef2cded041cf8c554451507d5e
Kartavya2015 <smita.rajan09@gmail.com>
2025-07-02 17:55:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+#empty dictionary
+my_dict = {}
 
-def circle_perimeter(radius):
- """Returns the perimeter (circumference) of a circle with given radius."""
- return 2 * math.pi * radius
+#dictionary with interger keys
+my_dict = {1: 'apple', 2: 'banana', 3: 'cherry'}
 
-def main():
-# Get radius as a floating-point number
- p = float(input("Enter the circle’s radius: "))
-# Calculate perimeter
-p = circle_perimeter()
-# Display result
-print(f"The perimeter of a circle with radius {p} is {p:.2f}")
+#dictionary with mixed keys
+my_dict = {'name': 'John', 1: [2, 4, 3]}
 
-if __name__ == "__main__":
- main()
+my_dict = {'name': "jack", 'age':26}
+
+#output
+print(my_dict['name'])
+print(my_dict['age'])
+
+#update value
+my_dict['age'] = 27
+print(my_dict)
+
+#add item
+my_dict['address'] = 'Downtown'
+print
+
+#remove paticular item
+my_dict.pop('age')
+print(my_dict)
+
+#accsessing keys
+print("Adress:", my_dict.get('address'))
+
+#remove all elements
+my_dict.clear()
+print(my_dict)

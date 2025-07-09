0903068e93f5c8ed825601945d21a2306da28791
Kartavya2015 <smita.rajan09@gmail.com>
2025-07-09 18:00:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-#empty tuple
-empty_tuple = ()
-my_tuple = ()
+#importing the module
+import sys
 
-#tuple with integers
-my_tuple = (1,2,3,4,5)
-
-#tuple with mixed data types
-my_tuple = (1, "jack", 3.5)
-
-#nested tuple
-nested_tuple = (1, 2, (3, 4), 5)
-
-#returning a tuple
-def return_tuple():
-    return (1, 2, 3)
-
-#accessing tuple elements using indexing
-my_tuple = ('bath', 'he he he', 'cancled', 'for', 'fun', 'today')
-print(my_tuple[1])
-print(my_tuple[5])
-
-#Iterating through a tuple
-for letter in (my_tuple[1:6]):
-    print("hello", letter)
+#this function will be the first to run as soon as the main function executes
+def initial_phonebook():
+    rows, cols = int (input("please enter initial number of contacts: ")), 5
+#we are collecting the initial number of contacts the user wants to have in the 
+#phone book already.                  user may add 0 if he/she doesn't wish to enter any.
+    phone_book = []
+    print(phone_book)
+    for i in range(rows):
+        print("\nEnter contact %d details in the following order(ONLY):" % (i + 1))
+        print("Note * indicates mandatory field")
+        print("***********************************************************")
+        temp = []
+        for j in range(cols):
+            #we have taken the conditions for values of j only for the personalized fields
+            #such as name, e-mail id, dob, category etc
+            if j == 0:
+                temp.append(str(input("Enter Name")))
+                #We need to check if the user has left the name empty as its mentioned that
+                                     #name and number are mandatory fields
+                                     #So implement a condition to check as bellow
+    if temp[j] == '' or temp[j] == '':
+     sys.exit(
+        "name is a mandatory field.Process exiting due to blank feild.....")
+     #this will exit the process if a blank field is encountered
+    if j == 1:
+       temp.append(int(input("Enter number")))
+       #we do not need to check if user has entered the number because int auto matically
+       #takes care of it.Int value canot accept a blank as that counts as a string.
+       #so prcees auto ma tically shuts down without us using the sys pakage
+       if j==2:

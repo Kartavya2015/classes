af8c01ea3ee81cd8241cf8cb703cdd5b8826c6a2
Kartavya2015 <smita.rajan09@gmail.com>
2025-07-24 21:31:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-new_file = open('index.html', 'x')
-new_file.close
+
 
 import os
 print("processing...")
 if os.path.exists("index.html"):
-    os.remove("my.py")
-
+    os.remove("index.htm")
+    new_file = open('index.htm', 'x')
+    new_file.close()
 else:
-    print("sorry,check for\ndoes file exist?\nis the name correct?\nwe can create new!")
+    print("sorry,check for:\ndoes file exist?\nis the name correct?\nwe can create new!")
 
-my_py = open("my_py", "w")
-my_py.write("hi nice to meet you\n i am siri")
-my_py.close()
+py = open("my_py", "w")
+py.write("hi nice to meet you\n i am siri")
+py.close()
 
 os.remove('index.html')
-os.rmdir('part 2')

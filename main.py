a19f457a103959a87042922bc94ba1794e7735f0
Kartavya2015 <smita.rajan09@gmail.com>
2025-07-28 18:13:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
-import random
-attempts_list = []
-def show_score():
-    if len(attempts_list) <= 0:
-        print("Hey,you might be new here, please play the game first!")
-    else:
-        print("yor score is {} attempt/s!".format(min(attempts_list)))
-        def start_game():
-            random_number =int(random.randint(1, 10))
-            print("hey there.whats yor name?")
-            player_name = input("Enter right HERE:")
-            wanna_play = input("Hey {}, do you want to play a game? (yes/no): ".format(player_name))
-            ## Where the show_score function USED to be 
-            attempts = 0
-            while wanna_play.lower() == "yes":
-                try:
-                    guess = input("Enter a number between 1 and 10: ")
-                    if int(guess)< 1 or int(guess) > 10:
-                        raise ValueError("Number out of range. Please try again in 1-10.")
-                    if int(guess) == random_number:
-                        print("congrats,you geussed it right!")
-                        attempts +=1
-                        attempts_list.append(attempts)
-                        print("You guessed it in {} attempts.".format(attempts))
-                        play_again = input("do you want to play again? (yes/no): ")
-                        attempts = 0
-                        show_score()
-                        random_number = int(random.randint(1-10))
-                        if play_again.lower() == "no":
-                            print("that's col,have a nice day")
-                            break
-                        elif int(guess) > random_number:
-                            print("Your guess is too high, try again!")
-                        elif int(guess) < random_number:
-                            print("Your guess is too low, try again!")
-                except ValueError as err:
-                    print("Invalid input")
-                    print("({})".format(err))
-            else:
-                print("Thanks for playing, !")
-                if __name__ == '__main__' :
-                 start_game()
+def calculator():
+    while True:
+        print("\n=== Simple Calculator ===")
+        print("1. Addition (+)")
+        print("2. Subtraction (-)")
+        print("3. Multiplication (*)")
+        print("4. Division (/)")
+        print("5. Modulo (%)")
+        print("6. Exit")
+
+        choice = input("Choose an operation (1-6): ")
+
+        if choice == '6':
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if choice in ['1', '2', '3', '4', '5']:
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+
+                if choice == '1':
+                    result = num1 + num2
+                    print(f"Result: {num1} + {num2} = {result}")
+                elif choice == '2':
+                    result = num1 - num2
+                    print(f"Result: {num1} - {num2} = {result}")
+                elif choice == '3':
+                    result = num1 * num2
+                    print(f"Result: {num1} * {num2} = {result}")
+                elif choice == '4':
+                    if num2 != 0:
+                        result = num1 / num2
+                        print(f"Result: {num1} / {num2} = {result}")
+                    else:
+                        print(" Error: Division by zero is not allowed.")
+                elif choice == '5':
+                    if num2 != 0:
+                        result = num1 % num2
+                        print(f"Result: {num1} % {num2} = {result}")
+                    else:
+                        print(" Error: Modulo by zero is not allowed.")
+            except ValueError:
+                print(" Invalid input. Please enter numbers only.")
+        else:
+            print(" Invalid choice. Please select from 1 to 6.")
+
+# Run the calculator
+calculator()

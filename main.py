2c588ed76e1cf466b2ed5a8ef41d5cf2c7017307
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-17 12:11:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Kartavya")
-print("Hello, I am in grade 5")
+#print a scentence
+print("Welcome to the world of programming!")
+
+#print a number
+print(46)
+
+#print newline
+print("Hello Kartavya. \n")
+
+#print multiple arguments for us
+print("Kartavya", 10)
+
+#end arguments of print statment
+print("My school name is Vibgyor High", end="*")

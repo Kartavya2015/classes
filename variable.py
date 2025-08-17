900ff0336e6f72064982dd6e2b696d397877a771
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-17 12:24:26 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,14 @@
+#Creating diffrent variables
+x = 10
+y = "Kartavya"
+print(x)
+print(y)
+
+school = "Vibgyor high"
+print(school)
+
+#user input
+age = input("What is your age: ")
+name = input("What is your name: ")
+school1 = input("what is your school name: ")
+print("\nHello", age,"\n", name,"\n", school1,"\n This is all about me.")

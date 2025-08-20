2278762dbb8c30bdde08c4609e3ce3422e6e49b6
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-20 17:44:03 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,22 @@
+name = "Kartavya"
+age = 10
+weight = 29.9
+is_student = True
+print("Name:", name)
+print("data type of name is", type(name))
+print("age:", age)
+print("data type of age is", type(age))
+print("weight:", weight)
+print("data type of weight is", type(weight))
+print("is_student:", is_student)
+print("data type of is_student is", type(is_student))
+
+print("\n After typecasting...")
+age = str(age)
+print(age)
+print("data type of age is", type (age))
+
+
+weight = int(weight)
+print(weight)
+print("data type of weight is:", type(weight))

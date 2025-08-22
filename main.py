8ce31c8f423fd9441c41a0519dc08357f51465bb
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-22 21:47:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-#First persons birthday
-paul_birthday = 7, "october"
-
-#Second persons birthday
-mary_birthday = 7, "may"
-
-#third persons birthday
-john_birthday = 7, "june"
-
-#fourth persons birthday
-kiara_birthday = 25, "september"
-
-#fifth persons birthday
-Aarav_birthday = 10, "July"
-#time to print the variables
-print("Paul's birthday is on:", paul_birthday,"\n")
-print("Mary's birthday is on:", mary_birthday,"\n")
-print("John's birthday is on:", john_birthday,"\n")
-print("Kiara's birthday is on:", kiara_birthday,"\n")
-print("Aarav's birthday is on:", Aarav_birthday, end='*')
+letter_Congrats = "Hey,I saw your place.I am very happy."
+letter_Congrats2 = "Keep up the good work"
+print(letter_Congrats + letter_Congrats2, "This is a", type(letter_Congrats),"and next part is...",  type(letter_Congrats2))

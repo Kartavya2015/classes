a4db6521f5028ca5bc077200002a16d852d15f9e
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-24 12:04:43 +0530

    Add files via upload

---
+++ avarage.py
@@ -0,0 +1,11 @@
+tree1 = 45
+tree2 = 67
+tree3 = 56
+tree4 = 103
+tree5 = 89
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("The total sum  is ", sum)
+
+#Finding the average
+average = sum/5
+print("the average is ", average)

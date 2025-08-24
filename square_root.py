b90d07bde738ab942b33133f18daa516d1f57dda
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-24 18:19:57 +0530

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,5 @@
+num = int(input("Enter your number to find square root[Note:Keep simple numbers or eroor will occur ]: "))
+def Square_roots():
+    print("Okay. Time to find some SQUARE ROOTS!")
+result = num ** 0.5
+print("The square root of ", num, "is ", result, ".")

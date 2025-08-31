44e8ac09ee7f8a3e6c53defda43c90076dbf9636
Kartavya2015 <smita.rajan09@gmail.com>
2025-08-31 12:22:22 +0530

    Add files via upload

---
+++ grater_or_smaller.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+
+if (num > 15):
+    print("The number is grater than 15")
+    print("I'm a if block")
+else:
+    print("the number is smaller than 15")
+    print("I'm a else block")
+
+print("I'm not a if block nor a else block!")

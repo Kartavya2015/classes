cb5bd26db06bc52426d4069e16d2732b45c9dbfb
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-01 17:22:27 +0530

    Add files via upload

---
+++ outside.py
@@ -0,0 +1,10 @@
+weather = str(input("What's the weather like outside: "))
+
+if weather == "suuny" or weather == "hot":
+    print("Don't forget your thin green T-shirt!")
+elif weather == "rainy" or weather == "monsoon":
+    print("Don't forget your umbrella and your rain coat[Maybe a jackect as well]!")
+elif weather == "cold" or weather == "snowy":
+    print("Don't forget your warm clothes and a jacket!")
+else:
+    print("Did you type anything wrong?If you typed normal,then I don't know what to suggest you!")

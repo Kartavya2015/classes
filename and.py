eb94d4e53c65d84a51cf1b67986d71dc797423b9
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-07 12:01:07 +0530

    Add files via upload

---
+++ and.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+c = 0
+
+if a and b and c:
+    print("All numbers have boolean values as True")
+else:
+    print("At least 1 number has boolean value as false")
+
+a = -10
+b = -20
+c = 0
+if a > 0 or b > 0 or c > 0:
+    print("Ehiter of the number is greater than 0")
+else:
+    print("At least 1 number is not greater than 0")

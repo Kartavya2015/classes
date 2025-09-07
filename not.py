488133761a0eaac75bfcf1ddd461ef27ec3f1abe
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-07 12:14:30 +0530

    Add files via upload

---
+++ not.py
@@ -0,0 +1,25 @@
+a = 30
+b = 20
+c = 7
+
+print(a != b)
+print(b != c)
+
+i = "kartavya"
+h = "patel"
+
+if i != h:
+    print("i and h are not equal")
+i == 78
+h == 78
+
+if i != h:
+    print("k and l are not equal")
+else:
+    print("k and l are equal")
+
+num = int(input("Enter number: "))
+if num%2 != 0:
+    print("This is an odd number")
+else:
+    print("This is an even number")

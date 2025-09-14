427fed284ad9bbfd14aad84468ea2e5fdf17450e
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-14 12:02:17 +0530

    Add files via upload

---
+++ opreators_pesedence.py
@@ -0,0 +1,16 @@
+a = 8
+b = 89
+c = 78
+x = 2
+y = 9
+z = 0
+z = (a + b) * c / x ;
+print("Value of (a + b) X c / x is", z)
+
+name = "Kartavya"
+age = 10
+
+if name == "Kartavya" or name == "Aarav" and age >= 4:
+    print("Helo! Welcome")
+else:
+    print("Good bye!")

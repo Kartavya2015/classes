94f2a98c79d1ee7578d6dc50dbcea581739cc277
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-18 12:03:07 +0530

    Add files via upload

---
+++ grade_application.py
@@ -0,0 +1,11 @@
+print1 = int(input("Enter your age: "))
+print(print1)
+
+if (print1 >= 20):
+    print("You are allowed.")
+    if (print1 <= 10):
+        print("You are allowed.")
+    else:
+        print("You are too big.")
+else:
+    print("You are too small.")

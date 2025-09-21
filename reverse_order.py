791193beccc37bef112c92223b29efbad06ed1a6
Kartavya2015 <smita.rajan09@gmail.com>
2025-09-21 12:22:32 +0530

    Add files via upload

---
+++ reverse_order.py
@@ -0,0 +1,6 @@
+range1 = int(input("Enter of the value of the range: "))
+print(f"numbers from {0} to {1} are".format(range1, 1))
+
+#loop to print order
+for i in range(range1, 0, -1):
+    print(i)

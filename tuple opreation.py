0568768b1f5cd7a93074ed6982ec62aeaf15b889
Kartavya2015 <smita.rajan09@gmail.com>
2025-11-12 17:52:12 +0530

    Add files via upload

---
+++ tuple opreation.py
@@ -0,0 +1,16 @@
+tuplek = (2, 3, 4, 5 ,6, 7, 8, 9, 10)
+print(tuplek)
+
+tuplek = (2, 3.4, 'kartavya', True)
+print(tuplek)
+
+tuplek = (2, 2, 4, 5, 4, 5, 6, 7, 7, 6, 8, 2, 56, 4, 56, 2, 2, 2)
+print(tuplek.count(2))
+
+tuplek = (2, 3, 4, 5 ,6, 7, 8, 9, 10)
+_slice = tuplek[2:-2]
+print(_slice)
+
+tuplek = (2, 3, 4, 5 ,6, 7, 8, 9, 10)
+_slice = tuplek[:-2]
+print(_slice)

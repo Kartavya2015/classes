293a29eca6af37e3f35f1d0a8c609e7d29562032
Kartavya2015 <smita.rajan09@gmail.com>
2025-12-08 18:32:22 +0530

    Add files via upload

---
+++ IOSString.py
@@ -0,0 +1,17 @@
+#create class
+class IOSstring():
+ #cunstructor
+    def __init__(self):
+        self.str1 = ""
+#function
+    def input_string(self):
+        self.str1 = input("Enter a string: ")
+#print
+    def print_string(self):
+        print("The string you printed in uppercase is.", str1.str1.upper())
+#object
+str1 = IOSstring()
+
+#call function
+str1.input_string()
+str1.print_string()
